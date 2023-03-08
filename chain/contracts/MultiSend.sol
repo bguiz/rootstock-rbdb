c039394f8e68d96edaffcb3ef53d66c8c0e9be0a
bguiz <github@bguiz.com>
2023-03-08 23:54:37 +0800

    feat: implement multiSend of ERC20 tokens by pushing

--- chain/contracts/MultiSend.sol
+++ chain/contracts/MultiSend.sol
@@ -12,4 +12,45 @@ contract MultiSend
     constructor()
         Ownable()
     {}
+
+    function multiSendFungibleTokenPush(
+        IERC20 token,
+        uint256 amount,
+        address[] calldata recipients
+    )
+        public
+        onlyOwner
+    {
+        require(
+            address(token) != address(0),
+            "Invalid ERC20 address"
+        );
+        require(
+            amount > 0,
+            "Zero amount"
+        );
+        uint256 numRecipients = recipients.length;
+        require(
+            (numRecipients > 1) &&
+                (numRecipients <= MAX_COUNT),
+            "Invalid number of recipients"
+        );
+        // NOTE fail-fast approach - we can know upfront
+        // if the loop will throw part-way through
+        require(
+            token.allowance(msg.sender, address(this)) >
+                (numRecipients * amount),
+            "Insufficient ERC20 allowance"
+        );
+        for (uint256 idx = 0; idx < numRecipients; idx++) {
+            require(
+                token.transferFrom(
+                    msg.sender,
+                    recipients[idx],
+                    amount
+                ),
+                "ERC20 transfer failed"
+            );
+        }
+    }
 }
