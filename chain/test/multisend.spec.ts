7007cc95a56612fd2229f9d08a59ceb50c1d876a
bguiz <github@bguiz.com>
2023-03-09 22:49:36 +0800

    test: add tests for basic failure scenarios around require statements for multiSend.multiSendFungibleTokenPush

--- chain/test/multisend.spec.ts
+++ chain/test/multisend.spec.ts
@@ -3,6 +3,9 @@ import { expect } from 'chai';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { MultiSend__factory } from '../typechain-types/factories/contracts/MultiSend__factory';
 import { MultiSend } from '../typechain-types/contracts/MultiSend';
+import { RbdbFungibleToken__factory } from '../typechain-types/factories/contracts/RbdbFungibleToken__factory';
+import { RbdbFungibleToken } from '../typechain-types/contracts/RbdbFungibleToken';
+import { BigNumber, ContractTransaction } from 'ethers';
 
 describe('MultiSend', () => {
     let deployer: SignerWithAddress;
@@ -20,4 +23,47 @@ describe('MultiSend', () => {
         const [maxCount] = await multiSend.functions.MAX_COUNT();
         expect(maxCount).to.equal(64);
     });
+
+    describe('multiSendFungibleTokenPush', () => {
+
+        it('should fail when zero-address is passed as the token parameter', async () => {
+            const zeroAddress = hre.ethers.constants.AddressZero;
+            const invocation = multiSend.multiSendFungibleTokenPush(
+                zeroAddress,
+                0,
+                [deployer.address],
+            );
+            await expect(invocation).to.be.revertedWith('Invalid ERC20 address');
+        });
+
+        it('should fail when zero amount is passed as the token parameter', async () => {
+            const invocation = multiSend.multiSendFungibleTokenPush(
+                deployer.address,
+                0,
+                [deployer.address],
+            );
+            await expect(invocation).to.be.revertedWith('Zero amount');
+        });
+
+        it('should fail when an invalid number of recipients is passed', async () => {
+            const recipients = Array.from({ length: 65 }, (_, i) => deployer.address);
+            const invocation = multiSend.multiSendFungibleTokenPush(
+                deployer.address,
+                1,
+                recipients,
+            );
+            await expect(invocation).to.be.revertedWith('Invalid number of recipients');
+        });
+
+        it('should fail when an invalid number of recipients is passed', async () => {
+            const recipients = Array.from({ length: 1 }, (_, i) => deployer.address);
+            const invocation = multiSend.multiSendFungibleTokenPush(
+                deployer.address,
+                1,
+                recipients,
+            );
+            await expect(invocation).to.be.revertedWith('Invalid number of recipients');
+        });
+    });
+
 });
