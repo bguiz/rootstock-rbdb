c907f295b08775dbc94fba6cf06c73b2a74d69af
bguiz <github@bguiz.com>
2023-03-09 09:11:56 +0800

    docs: add @dev comments for MultiSend SC

--- chain/contracts/MultiSend.sol
+++ chain/contracts/MultiSend.sol
@@ -4,15 +4,33 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title MultiSend
+ * @dev This contract provides a single function to send
+ * the native coin on the network, or ERC20 fungible tokens,
+ * to multiple recipients using a single function invocation.
+ */
 contract MultiSend
     is Ownable
 {
+    /**
+     * @dev The maximum number of recipients that may receive a transfer
+     * within a single function invocation.
+     */
     uint256 constant MAX_COUNT = 64;
 
     constructor()
         Ownable()
     {}
 
+    /**
+     * @dev Sends ERC20 fungible tokens to multiple recipients.
+     * @param token The address of the ERC20 token to be sent.
+     * @param amount The amount of tokens to send.
+     *  Note that exact amount should be used, will **not** multiply by exponent
+     *  of `decimals()`.
+     * @param recipients An array of addresses to receive the tokens.
+     */
     function multiSendFungibleTokenPush(
         IERC20 token,
         uint256 amount,
@@ -54,6 +72,13 @@ contract MultiSend
         }
     }
 
+    /**
+     * @dev Sends native coin to multiple recipients.
+     * @param amount The amount of coins to send.
+     *  Note that exact amount should be used, will **not** multiply by exponent
+     *  of smallest unit to "whole" unit (e.g. 10^18).
+     * @param recipients An array of addresses to receive the coins.
+     */
     function multiSendNativeCoinPush(
         uint256 amount,
         address payable[] calldata recipients
@@ -79,6 +104,9 @@ contract MultiSend
             "Insufficient ERC20 allowance"
         );
         for (uint256 idx = 0; idx < numRecipients; idx++) {
+            // NOTE that `call` in combination with a re-entrancy guard is the
+            // recommended method to use as of 2019/12.
+            // `tansfer` and `send` are no longer considered secure.
             (bool didSend, /* bytes memory data */) =
                 recipients[idx].call{value: amount}("");
             require(
